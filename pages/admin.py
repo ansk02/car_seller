290a433079e3a3d41bb44cea27f4f0526bed78b7
Abdul kabirk KEITA <mamaisha728@gmail.com>
2021-07-12 16:40:01 +0100

    Team model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Team
+from django.utils.html import format_html
 # Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+
+	def thumbnail(self, object):
+		return format_html('<img src=" {} " width="40" style="border-radius:50px;" />'.format(object.photo.url))
+
+	thumbnail.short_description = 'Photo'
+
+	list_display = ('lastname', 'firstname','designation', 'thumbnail')
+	list_display_links = ('firstname', 'lastname', 'thumbnail')
+	search_fields = ('lastname', 'firstname', 'designation')
+	list_filter = ('designation',)
+
+
+admin.site.register(Team, TeamAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+	lastname         = models.CharField(max_length=100, verbose_name='Nom')
+	firstname        = models.CharField(max_length=100, verbose_name='Prénom')
+	designation      = models.CharField(max_length=100, verbose_name='Responsabilité')
+	photo            = models.ImageField(upload_to='photos/%Y/%m/%d')
+	facebook_link    = models.URLField(max_length=200, verbose_name = 'Facebook')
+	twitter_link     = models.URLField(max_length=200, verbose_name = 'Twitter')
+	google_link      = models.URLField(max_length=200, verbose_name = 'Google Plus')
+	created_date     = models.DateTimeField(auto_now_add=True, verbose_name='Date creation')
+
+
+	class Meta:
+		verbose_name = 'Equipe'
+		verbose_name_plural = 'Equipes'
+
+	def __str__(self):
+		return self.firstname
