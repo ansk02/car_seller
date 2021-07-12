b1deee9920201103fced4dcd645f4dbd92cafcb4
Abdul kabirk KEITA <mamaisha728@gmail.com>
2021-07-12 14:29:26 +0100

    templates & nav links

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,19 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
 	return render(request, 'pages/home.html')
+
+
+def about(request):
+	return render(request, 'pages/about.html')
+
+
+def services(request):
+	return render(request, 'pages/services.html')
+
+
+def cars(request):
+	return render(request, 'pages/cars.html')
+
+
+def contact(request):
+	return render(request, 'pages/contact.html')
